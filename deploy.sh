dfdb6c333a7cc388ddc5e68b70ffff8b24000faf
vedhavyas <vedhavyas.singareddi@gmail.com>
2023-08-26 19:28:51 +0200

    rebuilding site Sat Aug 26 19:28:51 CEST 2023

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+# add hugo changes
+git add ./
+
+# commit and push hugo changes
+git commit -m "$msg"
+
+git push origin master
+
+echo -e "\033[0;32mDeployed updates to GitHub.\033[0m"
